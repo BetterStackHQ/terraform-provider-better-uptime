d7c0be2ad05bb96cc147bb0592c077fbef094c14
Simon Rozsival <simon@rozsival.com>
2021-08-13 15:10:29 +0200

    Move code to the data file and remove the resource file

--- internal/provider/data_policy.go
+++ internal/provider/data_policy.go
@@ -37,6 +37,67 @@ func newPolicyDataSource() *schema.Resource {
 	}
 }
 
+type policy struct {
+	Name          *string   `json:"name,omitempty"`
+	RepeatCount   *int      `json:"repeat_count,omitempty"`
+	RepeatDelay   *int      `json:"repeat_delay,omitempty"`
+	IncidentToken *string   `json:"incident_token,omitempty"`
+}
+
+var policySchema = map[string]*schema.Schema{
+	"id": {
+		Description: "The ID of this Policy.",
+		Type:        schema.TypeString,
+		Computed:    true,
+	},
+	"name": {
+		Description: "The name of this Policy.",
+		Type:        schema.TypeString,
+		Required:    true,
+	},
+	"repeat_count": {
+		Description: "", // TODO
+		Type:     schema.TypeInt,
+		Optional: true,
+	},
+	"repeat_delay": {
+		Description: "", // TODO
+		Type:        schema.TypeInt,
+		Optional:    true,
+	},
+	"incident_token": {
+		Description: "", // TODO
+		Type:        schema.TypeString,
+		Required:    true,
+	},
+}
+
+func policyRef(in *policy) []struct {
+	k string
+	v interface{}
+} {
+	// TODO:  if reflect.TypeOf(in).NumField() != len([]struct)
+	return []struct {
+		k string
+		v interface{}
+	}{
+		{k: "name", v: &in.Name},
+		{k: "repeat_count", v: &in.RepeatCount},
+		{k: "repeat_delay", v: &in.RepeatDelay},
+		{k: "incident_token", v: &in.IncidentToken},
+	}
+}
+
+func policyCopyAttrs(d *schema.ResourceData, in *policy) diag.Diagnostics {
+	var derr diag.Diagnostics
+	for _, e := range policyRef(in) {
+		if err := d.Set(e.k, reflect.Indirect(reflect.ValueOf(e.v)).Interface()); err != nil {
+			derr = append(derr, diag.FromErr(err)[0])
+		}
+	}
+	return derr
+}
+
 type policyPageHTTPResponse struct {
 	Data []struct {
 		ID         string  `json:"id"`
