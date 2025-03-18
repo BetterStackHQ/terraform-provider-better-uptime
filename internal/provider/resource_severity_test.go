714d0fa36e06c0697bbd4f9f8c622dc8f31bc8ba
Petr Heinz <petr@betterstack.com>
2025-03-18 16:05:54 +0100

    U-5479 [severity] Send PATCH request with urgency_group_id only when group changes (#152)

--- internal/provider/resource_severity_test.go
+++ internal/provider/resource_severity_test.go
@@ -27,11 +27,11 @@ func TestResourceSeverity(t *testing.T) {
 				}
 
 				resource "betteruptime_severity" "this" {
-				  name         = "Terraform - Test"
-				  sms          = true
-				  call         = false
-				  email        = false
-				  push         = true
+				  name           = "Terraform - Test"
+				  sms            = true
+				  call           = false
+				  email          = false
+				  push           = true
 				  critical_alert = true
 				}
 				`,
@@ -55,11 +55,11 @@ func TestResourceSeverity(t *testing.T) {
 				}
 
 				resource "betteruptime_severity" "this" {
-				  name         = "Terraform - Call only"
-				  sms          = false
-				  call         = true
-				  email        = false
-				  push         = false
+				  name           = "Terraform - Call only"
+				  sms            = false
+				  call           = true
+				  email          = false
+				  push           = false
 				  critical_alert = false
 				}
 				`,
@@ -69,12 +69,13 @@ func TestResourceSeverity(t *testing.T) {
 					resource.TestCheckResourceAttr("betteruptime_severity.this", "call", "true"),
 					resource.TestCheckResourceAttr("betteruptime_severity.this", "email", "false"),
 					resource.TestCheckResourceAttr("betteruptime_severity.this", "push", "false"),
+					server.TestCheckCalledRequest("PATCH", "/api/v2/urgencies/1", `{"name":"Terraform - Call only","sms":false,"call":true,"email":false,"push":false,"critical_alert":false}`),
 				),
 				PreConfig: func() {
 					t.Log("step 2")
 				},
 			},
-			// Step 3 - make no changes, check plan is empty.
+			// Step 3 - set severity group.
 			{
 				Config: `
                 provider "betteruptime" {
@@ -82,19 +83,50 @@ func TestResourceSeverity(t *testing.T) {
 				}
 
 				resource "betteruptime_severity" "this" {
-				  name         = "Terraform - Call only"
-				  sms          = false
-				  call         = true
-				  email        = false
-				  push         = false
-				  critical_alert = false
+				  name              = "Terraform - Call only"
+				  sms               = false
+				  call              = true
+				  email             = false
+				  push              = false
+				  critical_alert    = false
+				  severity_group_id = 123
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "name", "Terraform - Call only"),
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "sms", "false"),
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "call", "true"),
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "email", "false"),
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "push", "false"),
+					resource.TestCheckResourceAttr("betteruptime_severity.this", "severity_group_id", "123"),
+					server.TestCheckCalledRequest("PATCH", "/api/v2/urgencies/1", `{"name":"Terraform - Call only","sms":false,"call":true,"email":false,"push":false,"critical_alert":false,"urgency_group_id":123}`),
+				),
+				PreConfig: func() {
+					t.Log("step 2")
+				},
+			},
+			// Step 4 - make no changes, check plan is empty.
+			{
+				Config: `
+                provider "betteruptime" {
+					api_token = "foo"
+				}
+
+				resource "betteruptime_severity" "this" {
+				  name              = "Terraform - Call only"
+				  sms               = false
+				  call              = true
+				  email             = false
+				  push              = false
+				  critical_alert    = false
+				  severity_group_id = 123
 				}`,
 				PlanOnly: true,
 				PreConfig: func() {
 					t.Log("step 3")
 				},
 			},
-			// Step 4 - destroy.
+			// Step 5 - destroy.
 			{
 				ResourceName:      "betteruptime_severity.this",
 				ImportState:       true,
